a8d5bfca8c110b19f98334b32dd698ea4a558476
bsfelizardo <bsfelizardo@up.edu.ph>
2022-04-16 03:09:37 +0000

    Updated regex for flag

--- src/utils/regex.ts
+++ src/utils/regex.ts
@@ -3,7 +3,7 @@ export const regex = {
         CHANNEL: /^<#([0-9]+)>$/,
         USER: /^<@([0-9]+)>$/,
     },
-    FLAG: /^\|?\|?(flag\{[A-Za-z0-9_'\s]+\})\|?\|?$/,
+    FLAG: /^\|?\|?(flag\{[A-Za-z0-9_'@!]+\})\|?\|?$/,
     CHALLENGE_ID: /0x([0-9a-f]{4})/,
     COMMAND: /^([\w|\-]+)/,
 };
